e0de61a74b77df9a822d0a5e5556bf2c20310141
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-02 16:27:21 +0700

    ready

--- app/src/main/java/com/example/fundamentalandroidgithubusers/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroidgithubusers/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.fundamentalandroidgithubusers
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = "Hello world!!!"
+        textView.text = getString(R.string.message)
     }
 }
