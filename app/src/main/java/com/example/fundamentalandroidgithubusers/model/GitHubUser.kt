b1bba4ea93f8726c71b73a1b0397b92f3b2b06eb
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-04 17:55:31 +0700

    detail

--- app/src/main/java/com/example/fundamentalandroidgithubusers/model/GitHubUser.kt
+++ app/src/main/java/com/example/fundamentalandroidgithubusers/model/GitHubUser.kt
@@ -1,12 +1,52 @@
 package com.example.fundamentalandroidgithubusers.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class GitHubUser(
-    val username: String,
-    val name: String,
-    val avatar: String,
-    val company: String,
-    val location: String,
+    val username: String?,
+    val name: String?,
+    val avatar: String?,
+    val company: String?,
+    val location: String?,
     val repository: Int,
     val follower: Int,
     val following: Int,
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(username)
+        parcel.writeString(name)
+        parcel.writeString(avatar)
+        parcel.writeString(company)
+        parcel.writeString(location)
+        parcel.writeInt(repository)
+        parcel.writeInt(follower)
+        parcel.writeInt(following)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GitHubUser> {
+        override fun createFromParcel(parcel: Parcel): GitHubUser {
+            return GitHubUser(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GitHubUser?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/fundamentalandroidgithubusers/screen/detail/DetailActivity.kt
+++ app/src/main/java/com/example/fundamentalandroidgithubusers/screen/detail/DetailActivity.kt
@@ -1,12 +1,51 @@
 package com.example.fundamentalandroidgithubusers.screen.detail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import com.example.fundamentalandroidgithubusers.R
+import com.example.fundamentalandroidgithubusers.model.GitHubUser
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_USER = "extra_user"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val user = intent.getParcelableExtra<GitHubUser>(EXTRA_USER)
+
+        text_name.text = user?.name
+        text_username.text = user?.username
+        text_company.text = user?.company
+        text_location.text = user?.location
+        text_repository.text = user?.repository.toString()
+        text_follower.text = user?.follower.toString()
+        text_following.text = user?.following.toString()
+
+        image_avatar.setImageResource(
+            this.resources.getIdentifier(
+                user?.avatar,
+                "drawable",
+                this.packageName
+            )
+        )
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
